772496c0cc9f68c9dc7059471e6d169e3dbebce6
Victor <viricaes@hotmail.com>
2011-01-27 21:14:10 +0100

    At the beginning

---
+++ src/net/jbabel/plugins/IPluginBase.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.jbabel.plugins;
+
+/**
+ *
+ * @author victor
+ */
+public interface IPluginBase {
+
+}
